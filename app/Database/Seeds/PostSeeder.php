ee7216bb20dfd69c4b6306068c2cdffacc3558e3
tasyanursitadewi <tasyanursita@gmail.com>
2021-09-30 13:09:05 +0700

    terbaru

---
+++ app/Database/Seeds/PostSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+	public function run()
+        {
+                $model = model('PostModel');
+
+				$kategori =['nature', 'programing', 'cat', 'sport', 'lifestyle', 'food'];
+
+				for($i = 0; $i < 6; $i++){
+                $model->insert([
+                        'judul'      => static::faker()->sentence(3),
+                        'slug' 		 => static::faker()->unique()->slug(2),
+						'author' 	 => static::faker()->name(),
+						'kategori'   => $kategori[0],
+						'deskripsi'  => static::faker()->text()
+                ]);
+        }
+	}
+}
---
+++ app/Models/PostModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class PostModel extends Model
+{
+	protected $table                = 'posts';
+	protected $primaryKey           = 'post_id';
+	protected $allowedFields        = ['judul', 'deskripsi', 'gambar', 'author', 'kategori', 'slug', 'created_at', 'updated_at'];
+	protected $useTimestamps        = true;
+
+
+
+}
