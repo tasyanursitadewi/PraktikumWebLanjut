7b222b2ca56d21ff15bc2bffd7710cbae36976a3
tasyanursitadewi <tasyanursita@gmail.com>
2021-11-06 14:04:14 +0700

    update again

--- app/Views/view_admin.php
+++ app/Views/view_admin.php
@@ -165,7 +165,7 @@
           <img src="/assets/adminlte/dist/img/user4-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">Tasya Nursita Dewi</a>
         </div>
       </div>
 
